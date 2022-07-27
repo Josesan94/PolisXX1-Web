f50c1ecc6d0d14a3a2d63e1293217be0b1db4751
Josesan94 <josesantillan2@hotmai>
2022-07-27 20:51:55 -0300

    add links to social media icons

--- Frontend/src/components/sidebarRedes/SidebarRedes.jsx
+++ Frontend/src/components/sidebarRedes/SidebarRedes.jsx
@@ -21,23 +21,23 @@ export const SidebarRedes = () => {
             
             <li>
                 <FaTwitterSquare className="sidebarIcon" />
-                <p><Link to="https://twitter.com/noprog" target="_blank">Seguinos en<br/>Twitter</Link></p>
+                <p><a href="https://twitter.com/XxiPolis" >Seguinos en<br/>Twitter</a></p>
             </li>
             <li>
                 <FaInstagramSquare className="sidebarIcon" />
-                <p><Link to="https://twitter.com/noprog" target="_blank">Seguinos en<br/>Instagram</Link></p>
+                <p><a href="https://www.instagram.com/polis_xxi/" >Seguinos en<br/>Instagram</a></p>
             </li>
             <li>
                 <FaLinkedin className="sidebarIcon" />
-                <p><Link to="https://www.linkedin.com/company/programacionnet" target="_blank">Conectanos en<br/>LinkedIn</Link></p>
+                <p><a href="https://www.linkedin.com/in/polis-xxi/" >Conectanos en<br/>LinkedIn</a></p>
             </li>
             <li>
                 <FaTelegram className="sidebarIcon" />
-                <p><Link to="http://www.telegram.com/programacionnet" target="_blank">Conectanos en<br/>Telegram</Link></p>
+                <p><a href="http://www.telegram.com/programacionnet" >Conectanos en<br/>Telegram</a></p>
             </li>
             <li>
                 <FaDiscord className="sidebarIcon" />
-                <p><Link to="http://www.discord.com/programacionnet" target="_blank">nuestro canal de<br/>Discord</Link></p>
+                <p><a href="http://www.discord.com/programacionnet" >nuestro canal de<br/>Discord</a></p>
             </li>
         </ul>
     </div>
